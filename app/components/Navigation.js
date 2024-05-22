b2e16173cd90891bf6bfa0fd5f467af3344f4f3d
sumitsetia <summitsetia@gmail.com>
2024-05-23 11:06:12 +1200

    Intial Commit

---
+++ app/components/Navigation.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Image from 'next/image';
+
+const Navigation = () => {
+    return (
+        <div className='flex flex-col text-white'>
+            <nav className='flex justify-between fixed w-full z-10 px-12 py-4 bg-black'>
+                <Image
+                    
+                 />
+                <h1>Placeholder</h1>
+                <h1>Placeholder</h1>
+            </nav>
+        </div>
+    );
+};
+
+export default Navigation;
