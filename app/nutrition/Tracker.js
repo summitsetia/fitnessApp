54baf6d04a406deb11781f20f41cef2e3a7e8d71
sumitsetia <summitsetia@gmail.com>
2024-06-11 11:15:36 +1200

    Adding Data to Supabase Table (Working)

--- app/nutrition/Tracker.js
+++ app/nutrition/Tracker.js
@@ -1,28 +1,34 @@
 'use client'
 import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
-import { supabase } from '../notes/page'
+import { createClient } from '../../utils/supabase/client';
 
 const Tracker = () => {
-    const [calories, setCalories] = useState("");
-    const [fetchError, setFetchError] = useState(null)
-    const API_KEY = "3cgZDgcBsGkJoitjS+y1hA==VfY5rwMR5sPyhFtW";
-    const item = "100g eggs";
+    const supabase = createClient()
 
+    const [calories, setCalories] = useState(0);
+    const [fetchError, setFetchError] = useState(null)
+    const API_KEY = process.env.REACT_APP_API_KEY;
+    const foodQuantity = "80g";
+    const foodName = "apple"
+    
     useEffect(() => {
-        Axios.get(`https://api.api-ninjas.com/v1/nutrition?query= + ${item}`, {
+        Axios.get(`https://api.api-ninjas.com/v1/nutrition?query=${foodQuantity} ${foodName}`, {
             headers: {
                 'X-Api-Key': API_KEY
             }
         }).then((res) => {
-            setCalories(res.data);
+            console.log("calories:", res.data[0].calories)
+            setCalories(res.data[0].calories);
         })
     }, []);
 
     const updateTable = async () => {
+        console.log("clicked")
+        console.log("calories from updateTable", calories)
         const { data, error } = await supabase
-            .from('item')
-            .insert([{item, calories}])
+            .from('food_log')
+            .insert({ calories: calories, foodQuantity: foodQuantity, foodName: foodName})
         
         if (error) {
             console.log(error)
@@ -37,8 +43,9 @@ const Tracker = () => {
     return (
         <div>
             <div className='pt-32'>
-                {calories.length > 0 ? <h1>{calories[0].calories} kcal</h1> : <h1>Loading...</h1>}
+                {calories > 0 ? <h1>{calories} kcal</h1> : <h1>Loading...</h1>}
                 <button onClick={updateTable}>Yo</button>
+                <h1>{fetchError}</h1>
             </div>        
         </div>
     )
--- app/nutrition/page.js
+++ app/nutrition/page.js
@@ -1,4 +1,3 @@
-'use client'
 import React from 'react';
 import Navigation from '../components/Navigation';
 import Tracker from './Tracker';
