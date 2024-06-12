a0ad7f8139dfea1d674edc97a93aa32225cede58
sumitsetia <summitsetia@gmail.com>
2024-06-13 10:16:44 +1200

    Sum Of Calories DIsplaying on Dashboard

--- app/components/Dashboard.js
+++ app/components/Dashboard.js
@@ -1,8 +1,42 @@
 'use client';
 import React from 'react';
+import { useEffect , useState } from 'react';
+import { createClient } from '../../utils/supabase/client';
 
 
 const Dashboard = () => {
+    const supabase = createClient()
+    const [fetchError, setFetchError] = useState(null)
+    const [data, setData] = useState(null)
+    const [totalCalories, setTotalCalories] = useState(0)
+
+    useEffect (() => {
+        const fetchData = async () => {
+            const { data, error } = await supabase
+            .from('food_log')
+            .select()
+
+            if(error) {
+                console.log(error)
+                setFetchError("There is an error")
+            }
+            
+            if(data) {
+                console.log(data)
+                setFetchError(null)
+                setData(data)
+
+                const total = data.reduce((sum, element) => sum + element.calories, 0);
+                setTotalCalories(total)
+            }
+        }
+
+
+
+        fetchData()
+
+
+    } , [])
 
     return (
         <div className='py-36 bg-[#FBFBFF]'> 
@@ -12,12 +46,12 @@ const Dashboard = () => {
             <div className='grid grid-cols-3 gap-4'>
                 <div className='border-2 border-solid h-32 w-96 bg-white flex flex-col items-center'>
                     <h1 className='font-bold'>Calories Consumed</h1>
-                    <h1></h1>
+                    <h1>{data && data[0] ? `${totalCalories} kcal` : '0 kcal'}</h1>
                     <p></p> 
                 </div>
                 <div className='border-2 border-solid h-32 w-96 bg-white flex flex-col items-center'> 
                     <h1 className='font-bold'>Protein Consumed</h1>
-                    <p></p>
+                    <p>{fetchError}</p>
                 </div>
                 <div className='border-2 border-solid h-32 w-96 bg-white flex flex-col items-center'>
                     <h1 className='font-bold'>Carbs Consumed</h1>
--- app/nutrition/Tracker.js
+++ app/nutrition/Tracker.js
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Axios from 'axios';
 import { createClient } from '../../utils/supabase/client';
 
@@ -9,26 +9,14 @@ const Tracker = () => {
     const [calories, setCalories] = useState(0);
     const [fetchError, setFetchError] = useState(null)
     const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
-    const foodQuantity = "80g";
-    const foodName = "apple"
-    
-    useEffect(() => {
-        Axios.get(`https://api.api-ninjas.com/v1/nutrition?query=${foodQuantity} ${foodName}`, {
-            headers: {
-                'X-Api-Key': API_KEY
-            }
-        }).then((res) => {
-            console.log("calories:", res.data[0].calories)
-            setCalories(res.data[0].calories);
-        })
-    }, []);
+    const [formData, setFormData] = useState({foodQuantity : "", foodName: ""})
 
-    const updateTable = async () => {
+    const updateTable = async (calories) => {
         console.log("clicked")
         console.log("calories from updateTable", calories)
         const { data, error } = await supabase
             .from('food_log')
-            .insert({ calories: calories, foodQuantity: foodQuantity, foodName: foodName})
+            .insert({ calories: calories, foodQuantity: formData.foodQuantity, foodName: formData.foodName})
         
         if (error) {
             console.log(error)
@@ -40,11 +28,51 @@ const Tracker = () => {
         }
     }
 
+    const handleChange = (event) => {
+        return setFormData(prevData => {
+            return {
+                ...prevData,
+                [event.target.name]: event.target.value
+            }   
+        })
+    }
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+        Axios.get(`https://api.api-ninjas.com/v1/nutrition?query=${formData.foodQuantity} ${formData.foodName}`, {
+            headers: {
+                'X-Api-Key': API_KEY
+            }
+        }).then((res) => {
+            setCalories(res.data[0].calories);
+            updateTable(res.data[0].calories)
+        })
+            
+        }
+    
     return (
         <div>
-            <div className='pt-32'>
+            <div className='pt-64'>
+                <form onSubmit={handleSubmit}>
+                    <input 
+                        type="text"
+                        placeholder="foodQuantity"
+                        onChange={handleChange}
+                        name="foodQuantity"
+                        value={formData.foodQuantity}
+                    />
+                    <input 
+                        type="text"
+                        placeholder="foodName"
+                        onChange={handleChange}
+                        name="foodName"
+                        value={formData.foodName}
+                    />
+                    <button type="submit">Hello</button>
+                </form>
                 {calories > 0 ? <h1>{calories} kcal</h1> : <h1>Loading...</h1>}
-                <button onClick={updateTable}>Yo</button>
                 <h1>{fetchError}</h1>
             </div>        
         </div>
@@ -52,3 +80,4 @@ const Tracker = () => {
 }
 
 export default Tracker;
+
