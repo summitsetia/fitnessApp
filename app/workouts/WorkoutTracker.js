b5c284bda2f99e2f8fdad1a34ea7a74227813cea
summitsetia <summitsetia@gmail.com>
2024-06-23 23:47:17 +1200

    List For Excercise Start

--- app/workouts/WorkoutTracker.js
+++ app/workouts/WorkoutTracker.js
@@ -1,16 +1,18 @@
 'use client'
 import React, { useState } from "react";
 import { createClient } from '../../utils/supabase/client';
+import excerciseData from './excerciseData'
 
 const WorkoutTracker = () => {
     const supabase = createClient();
-    const [formData, setFormData] = useState({ weight: 0, reps: 0 });
+    const [formData, setFormData] = useState({ weight: "", reps: "" });
     const [submittedData, setSubmittedData] = useState({ weight: 0, reps: 0 });
+    const [showDropdown, setShowDropdown] = useState(false)
 
-    const updateTable = async ( submittedData ) => {
+    const updateTable = async ( userData ) => {
         const {data, error} = await supabase
         .from("workouts")
-        .insert({weight: submittedData.weight, reps: submittedData.reps})
+        .insert({weight: userData.weight, reps: userData.reps})
 
         if (error) {
             console.log(error)
@@ -32,8 +34,9 @@ const WorkoutTracker = () => {
     const handleSubmit = (event) => {
         event.preventDefault();
         setSubmittedData(formData);
-        updateTable(setSubmittedData)
+        updateTable(submittedData)
     };
+    
 
     return (
         <div className="py-64">
@@ -45,6 +48,8 @@ const WorkoutTracker = () => {
                     name="weight"
                     value={formData.weight}
                     className='border'
+                    onFocus={() => setShowDropdown(true)}
+                    onBlur={() => setShowDropdown(false)}
                 />
                 <input
                     type="text"
@@ -58,8 +63,26 @@ const WorkoutTracker = () => {
             </form>
             {submittedData.weight > 0 ? <h1 className="border">{submittedData.weight} kg</h1> : <h1></h1>}
             {submittedData.reps > 0 ? <h1 className="border">{submittedData.reps}</h1> : <h1></h1>}
+            <DropDown showDropdown={showDropdown}/>
         </div>
     );
 };
+
+const DropDown = ({ showDropdown }) => {
+    return (
+        <div>
+            {excerciseData.map((exercise) => {
+                return (
+                    showDropdown && (
+                        <div>
+                            <h1>{exercise.exercise}</h1>
+                        </div>
+                    )
+                );
+            })}
+        </div>
+    );
+};
+
 
 export default WorkoutTracker;
---
+++ app/workouts/excerciseData.js
@@ -0,0 +1,18 @@
+const excerciseData = [
+    {
+        id: 1,
+        excercise: "Bench Press",
+    },
+    {
+        id:2,
+        excercise: "Incline Dumbell Press",
+    },
+    {
+        id:3,
+        excercise: "Flat Dumbell Press",
+    },
+    {
+        id:4,
+        excercise: "Hack Squat",
+    }
+]
