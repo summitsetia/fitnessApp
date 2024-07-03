679dd4932a2b698860d61fd0319a856007e74d62
sumitsetia <summitsetia@gmail.com>
2024-07-03 13:23:06 +1200

    Workouts Page - Before Making new rows for sets

--- app/workouts/WorkoutTracker.js
+++ app/workouts/WorkoutTracker.js
@@ -9,9 +9,10 @@ const WorkoutTracker = () => {
     const [formData, setFormData] = useState({ weight: "", reps: "", excercise: "", sets: "" });
     const [submittedData, setSubmittedData] = useState({ weight: 0, reps: 0, excercise: "", sets: 0 });
     const [showDropdown, setShowDropdown] = useState(false);
-    const [showForm, setShowForm] = useState(false)
-    const [showSets, setShowSets] = useState(false)
-    
+    const [showForm, setShowForm] = useState(false);
+    const [showSets, setShowSets] = useState(false);
+    const [setNumber, setSetNumber] = useState(1);
+
     const updateTable = async (userData) => {
         const { data, error } = await supabase
             .from('workouts')
@@ -38,8 +39,8 @@ const WorkoutTracker = () => {
         event.preventDefault();
         setSubmittedData(formData);
         updateTable(formData);
-        setFormData(initialFormData); 
-        setShowDropdown(false); 
+        setFormData(initialFormData);
+        setShowDropdown(false);
     };
 
     const handleExerciseClick = (excercise) => {
@@ -48,63 +49,74 @@ const WorkoutTracker = () => {
             excercise: excercise
         }));
         setShowDropdown(false);
+        setShowSets(true); 
     };
 
     const handleFormClick = () => {
-        setShowForm(prevValue => !prevValue)
+        setShowForm(prevValue => !prevValue);
     }
 
     return (
         <div className="py-64">
             <button onClick={handleFormClick}>Start An Empty Workout</button>
             <div>
-                { showForm && (<form onSubmit={handleSubmit}>
-                    <input
-                        type="text"
-                        placeholder="excercise"
-                        onChange={handleChange}
-                        name="excercise"
-                        value={formData.excercise}
-                        className='border'
-                        onFocus={() => setShowDropdown(true)}
-                        onBlur={() => setShowDropdown(false)}
-                    />
-                    <input
-                        type="text"
-                        placeholder="sets"
-                        onChange={handleChange}
-                        name="sets"
-                        value={formData.sets}
-                        className='border'
-                    />
-                    <input
-                        type="text"
-                        placeholder="weight"
-                        onChange={handleChange}
-                        name="weight"
-                        value={formData.weight}
-                        className='border'
-                    />
-                    <input
-                        type="text"
-                        placeholder="reps"
-                        onChange={handleChange}
-                        name="reps"
-                        value={formData.reps}
-                        className="border"
-                    />
-                    <button type="submit" className='border'>Submit</button>
-                </form>
+                {showForm && (
+                    <form onSubmit={handleSubmit} className="flex flex-col">
+                        <input
+                            type="text"
+                            placeholder="excercise"
+                            onChange={handleChange}
+                            name="excercise"
+                            value={formData.excercise}
+                            className='border'
+                            onFocus={() => setShowDropdown(true)}
+                            onBlur={() => setShowDropdown(false)}
+                        />
+                        {showSets && (
+                            <>
+                                {/* <input
+                                    type="text"
+                                    placeholder="sets"
+                                    onChange={handleChange}
+                                    name="sets"
+                                    value={formData.sets}
+                                    className='border'
+                                /> */}
+                                
+                                <div className="flex flex-row">
+                                    <h1 className="pr-8">Set {setNumber}</h1>
+                                    <input
+                                        type="text"
+                                        placeholder="weight"
+                                        onChange={handleChange}
+                                        name="weight"
+                                        value={formData.weight}
+                                        className='border'
+                                    />
+                                    <input
+                                        type="text"
+                                        placeholder="reps"
+                                        onChange={handleChange}
+                                        name="reps"
+                                        value={formData.reps}
+                                        className="border"
+                                    />
+                                </div>
+                                <button onClick={() => setSetNumber(setNumber +1)}>Add More Sets</button>
+                                <button type="submit" className='border'>Submit</button>
+                            </>
+                        )}
+                    </form>
                 )}
-                {submittedData.excercise.length > 0 ? <h1 className="border">{submittedData.excercise} </h1> : <h1></h1>}
-                {submittedData.sets > 0 ? <h1 className="border">{submittedData.sets} </h1> : <h1></h1>}
+                {submittedData.excercise.length > 0 ? <h1 className="border">{submittedData.excercise}</h1> : <h1></h1>}
+                {submittedData.sets > 0 ? <h1 className="border">{submittedData.sets}</h1> : <h1></h1>}
                 {submittedData.weight > 0 ? <h1 className="border">{submittedData.weight} kg</h1> : <h1></h1>}
                 {submittedData.reps > 0 ? <h1 className="border ">{submittedData.reps}</h1> : <h1></h1>}
                 <div>
                     {showDropdown && (
-                        <DropDown handleExerciseClick={handleExerciseClick}/>
+                        <DropDown handleExerciseClick={handleExerciseClick} />
                     )}
-                </div> 
+                </div>
             </div>
         </div>
     );
