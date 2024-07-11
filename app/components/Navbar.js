6704461f9fcb8860c64fa05a56e12e710a98d0bc
summitsetia <summitsetia@gmail.com>
2024-07-12 03:14:29 +1200

    Shadcn Setup

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -3,6 +3,14 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import logout from "../logout/actions";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const Navbar = () => {
   return (
@@ -21,7 +29,18 @@ const Navbar = () => {
             Nutrition
           </Link>
         </div>
-        <button onClick={() => logout()}>Logout</button>
+        <button onClick={() => logout}>Logout</button>
+        <DropdownMenu>
+          <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </nav>
     </div>
   );
