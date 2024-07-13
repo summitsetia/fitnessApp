2a58affe34b259aed3583d57999c79a447b1c385
summitsetia <summitsetia@gmail.com>
2024-07-13 19:33:54 +1200

    logout with icon in navbar

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -11,12 +11,14 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { User } from "lucide-react";
 
 const Navbar = () => {
   return (
     <div className="sticky top-0 text-black ">
-      <nav className="flex w-full px-12 border-b-2 border-solid bg-white items-center h-">
-        <div className="items-center py-4">
+      <nav className="flex w-full px-12 border-b-2 border-solid bg-white items-center ">
+        <div className="py-4">
           <Link href="/" className="">
             <Image src="/images/logo.png" alt="Logo" width={75} height={75} />
           </Link>
@@ -28,22 +30,31 @@ const Navbar = () => {
           <Link className="text-lg font-semibold" href="/nutrition">
             Nutrition
           </Link>
+          <div className="">
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Button variant="ghost" size="icon">
+                  <User className="h-6 w-6" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Profile</DropdownMenuItem>
+                <DropdownMenuItem>Settings</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem
+                  onSelect={() => logout()}
+                  className="text-red-600"
+                >
+                  Logout
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
         </div>
-        <button onClick={() => logout}>Logout</button>
-        <DropdownMenu>
-          <DropdownMenuTrigger>Open</DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Profile</DropdownMenuItem>
-            <DropdownMenuItem>Billing</DropdownMenuItem>
-            <DropdownMenuItem>Team</DropdownMenuItem>
-            <DropdownMenuItem>Subscription</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
       </nav>
     </div>
   );
 };
-
 export default Navbar;
